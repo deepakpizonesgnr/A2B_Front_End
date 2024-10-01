181f34043e2883cd9d162f209f54f5e0abc95c7b
Deepak <deepakchoudhary@pizoneinfotech.com>
2024-10-01 14:18:50 +0530

    commit with architecture setup

---
+++ src/app/Core/app.routes.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../Feature/login/component/login.component';
+import { NotFoundComponent } from './404-page/not-found/not-found.component';
+
+
+export const routes: Routes = [  {
+    path: 'dashboard',
+    loadChildren: () => import('../Feature/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  
+  // Add other loaded modules here
+  { path: '', component: LoginComponent }, // Default route that redirects to login
+  { path: '**', component: NotFoundComponent } // Wildcard route to catch any invalid URLs
+ 
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ src/app/Shared/UI-Elements/input/imput/imput.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imput',
+  standalone: true,
+  imports: [],
+  templateUrl: './imput.component.html',
+  styleUrl: './imput.component.scss'
+})
+export class ImputComponent {
+
+}
